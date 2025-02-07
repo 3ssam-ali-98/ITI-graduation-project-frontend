6a8a7c0998f07d2392eca79e36154692149d29cb
YoustinaWilliam <126955785+YoustinaWilliam@users.noreply.github.com>
2025-02-07 22:45:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
   return (
     <Router>
       <Switch>
-        {/* <Route exact path="/" component={ClientTable} />
-        <Route path="/company-details/:id" component={CompanyDetails} /> */}
-
         <Route path="/:bussiness_id/clients" component={ClientTable} exact/>
         <Route path="/:bussiness_id/clients/company-details/:id" component={CompanyDetails} exact/>
       </Switch>
