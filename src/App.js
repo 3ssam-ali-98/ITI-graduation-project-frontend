296cc7e1ba53fe781e56ce261cff53d0a38976ee
“Essam” <essamali7698@gmail.com>
2025-02-08 15:40:45 +0200

    after 2nd merge

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import Navbar from './pages/Navbar';
-import HeroSection from './pages/HeroSection';
 import Services from './pages/Services';
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
-import Footer from './pages/Footer';
 import Login from './pages/login';
 import Register from './pages/regeister';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from './pages/landingpage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <HeroSection />
-        <Services />
-        <AboutUs />
-        <ContactUs />
-        <Footer />
-    <div className='d-flex contaner-fluid flex-wrap justify-content-center'>
         <Switch>
+          <Route path="/" component={LandingPage } exact />
           <Route path="/services" component={Services } exact />
           <Route path="/about" component={AboutUs } exact />
           <Route path="/contact" component={ContactUs } exact />
+          <div className='d-flex container-fluid flex-wrap align-content-center justify-content-center'>
             <Route exact path="/login" component={Login}/>
             <Route exact path="/register" component={Register}/>
+          </div>
         </Switch>
-    </div>
       </BrowserRouter>
     </div>
   );
